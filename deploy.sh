7b5b271fce669fe0872c6cb485f409db39e1e6c6
ligulfzhou <ligulfzhou53@gmail.com>
2023-08-11 14:42:32 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
-
-ssh hosthatch <<EOF
-ssh ovh <<EOF
+ssh hosthatch << EOFHOSTHATCH
+ssh ovh << EOFOVH
 cd /home/debian/erp-api
 /home/debian/.cargo/bin/cargo build -r
 /usr/bin/svc restart erp-9100
-EOF
-EOF
+EOFOVH
+EOFHOSTHATCH
