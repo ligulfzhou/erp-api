e67d526452f88795113b5f3e9fa24382e1fb1100
ligulfzhou <ligulfzhou53@gmail.com>
2023-08-15 12:41:12 +0800

    add constant: STORAGE_URL_PREFIX

--- src/constants.rs
+++ src/constants.rs
@@ -2,3 +2,6 @@ pub const DEFAULT_PAGE_SIZE: i32 = 50;
 
 pub const STORAGE_FILE_PATH: &str = "/home/debian/data/file/";
 // pub const STORAGE_FILE_PATH: &str = "/Users/ligangzhou/data/file/";
+
+pub const STORAGE_URL_PREFIX: &str = "https://erp.ligulfzhou.com/file/";
+// pub const STORAGE_URL_PREFIX: &str = "http://localhost:9100/file/";
