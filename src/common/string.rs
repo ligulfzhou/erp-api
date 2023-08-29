51e13cd66d203e3bd70fae8c0c3d02cccc33d90e
ligulfzhou <ligulfzhou53@gmail.com>
2023-08-29 22:41:04 +0800

    common_prefix

--- src/common/string.rs
+++ src/common/string.rs
@@ -1,5 +1,58 @@
-pub fn common_prefix(strs: Vec<&str>) -> String {
-    "".to_string()
+pub fn common_prefix(strs: Vec<String>) -> Option<String> {
+    if strs.is_empty() {
+        return None;
+    }
+
+    let mut res = "".to_string();
+    let mut i = 0;
+    loop {
+        let mut c = None;
+        for str in &strs {
+            if i == str.len() {
+                return Some(res);
+            }
+
+            match c {
+                None => {
+                    c = Some(str.as_bytes()[i]);
+                }
+                Some(letter) if letter != str.as_bytes()[i] => return Some(res),
+                _ => continue,
+            }
+        }
+        if let Some(letter) = c {
+            res.push(char::from(letter));
+        }
+
+        i += 1;
+    }
+}
+
+fn lcp(list: &[&[u8]]) -> Option<Vec<u8>> {
+    if list.is_empty() {
+        return None;
+    }
+    let mut ret = Vec::new();
+    let mut i = 0;
+    loop {
+        let mut c = None;
+        for word in list {
+            if i == word.len() {
+                return Some(ret);
+            }
+            match c {
+                None => {
+                    c = Some(word[i]);
+                }
+                Some(letter) if letter != word[i] => return Some(ret),
+                _ => continue,
+            }
+        }
+        if let Some(letter) = c {
+            ret.push(letter);
+        }
+        i += 1;
+    }
 }
 
 pub fn remove_whitespace_string(s: &mut String) {
@@ -8,4 +61,20 @@ pub fn remove_whitespace_string(s: &mut String) {
 
 pub fn remove_whitespace_str(s: &str) -> String {
     s.chars().filter(|c| !c.is_whitespace()).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::common::string::common_prefix;
+
+    #[test]
+    fn test_lcp() -> anyhow::Result<()> {
+        let cp = common_prefix(vec![
+            "helloworld".to_string(),
+            "hellobob".to_string(),
+            "hellxyz".to_string(),
+        ]);
+        println!("cp: {:?}", cp);
+        Ok(())
+    }
 }
