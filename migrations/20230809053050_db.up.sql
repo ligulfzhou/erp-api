58b7710bb677b938434983eb190dee700f7ed462
ligulfzhou <ligulfzhou53@gmail.com>
2023-10-27 16:20:32 +0800

    update schema

--- migrations/20230809053050_db.up.sql
+++ migrations/20230809053050_db.up.sql
@@ -2,15 +2,15 @@
 create table goods
 (
     id               SERIAL,
-    customer_no      text not null default '',   -- 客户ID
-    goods_no         text not null default '',   -- 类目编号(可以为空，主要来自L1005)
-    images           text[] not null default '{}', -- 图片
-    image_des        text not null default '',   -- 图片
-    name             text not null default '',   -- 名称
-    plating          text not null default '',   -- 电镀
-    package_card     text not null default '',   -- 标签图片
-    package_card_des text not null default '',   -- 标签说明
-    notes            text not null default ''    -- 备注
+    customer_no      text not null default '',     -- 客户ID
+    goods_no         text not null default '',     -- 类目编号(可以为空，主要来自L1005)
+    images           text[] not null default '{}', -- 图片 TODO: 只记录最后出现的图片
+    image_des        text not null default '',     -- 图片
+    name             text not null default '',     -- 名称
+    plating          text not null default '',     -- 电镀
+    package_card     text not null default '',     -- 标签图片
+    package_card_des text not null default '',     -- 标签说明
+    notes            text not null default ''      -- 备注
 );
 create index idx_goods_customer_no on goods (customer_no);
 create unique index uniq_goods_goods_no on goods (goods_no);
@@ -82,7 +82,6 @@ values ('L1012', 3);
 insert into customer_excel_template (customer_no, template_id)
 values ('L1004', 4);
 
-
 -- 订单表
 create table orders
 (
@@ -102,10 +101,14 @@ create index idx_orders_customer_no_order_date on orders (customer_no, order_dat
 -- 订单商品表
 create table order_goods
 (
-    id       serial,
-    index    integer not null default 0, -- 用于排序
-    order_id integer not null,           -- 订单ID
-    goods_id integer not null            -- 商品ID
+    id               serial,
+    index            integer not null default 0,   -- 用于排序
+    images           text[] not null default '{}', -- 图片 TODO: 只记录最后出现的图片
+    image_des        text    not null default '',  -- 图片
+    package_card     text    not null default '',  -- 标签图片
+    package_card_des text    not null default '',  -- 标签说明
+    order_id         integer not null,             -- 订单ID
+    goods_id         integer not null              -- 商品ID
 );
 create index idx_order_goods_order_id on order_goods (order_id);
 create unique index uniq_order_goods_order_id_and_goods_id on order_goods (order_id, goods_id);
@@ -114,21 +117,21 @@ create unique index uniq_order_goods_order_id_and_goods_id on order_goods (order
 create table order_items
 (
     id             serial,
-    order_goods_id integer not null,           -- 订单商品ID
-    order_id       integer not null,           -- 订单ID
-    sku_id         integer not null,           -- sku id
-    count          integer not null,           -- 数量
-    unit           text,                       -- 单位
-    unit_price     integer,                    -- 单价
-    total_price    integer,                    -- 总价/金额
-    notes          text    not null default '' -- 备注
+    order_goods_id integer not null,             -- 订单商品ID
+    order_id       integer not null,             -- 订单ID
+    sku_id         integer not null,             -- sku id
+    count          integer not null,             -- 数量
+    unit           text,                         -- 单位
+    unit_price     integer,                      -- 单价
+    total_price    integer,                      -- 总价/金额
+    notes_images   text[] not null default '{}', -- 备注内的图片
+    notes          text    not null default ''   -- 备注
 );
---     goods_id       integer not null, -- 商品ID
+-- goods_id       integer not null, -- 商品ID
 -- create index idx_order_items_goods_id on order_items (goods_id);
 create index idx_order_items_order_id on order_items (order_id);
 create index idx_order_items_sku_id on order_items (sku_id);
 create unique index uniq_order_items_order_id_and_sku_id on order_items (order_id, sku_id);
-
 
 create table progress
 (
@@ -183,6 +186,6 @@ create table accounts
 insert into accounts (name, account, password, department_id)
 values ('业务test', 'test', 'test', 1);
 insert into accounts (name, account, password, department_id)
-values ('业务小红', 'yewuxiaobai', 'yewuxiaobai', 1);
+values ('业务小白', 'yewuxiaobai', 'yewuxiaobai', 1);
 insert into accounts (name, account, password, department_id)
 values ('仓库小黄', 'cangkuxiaohuang', 'cangkuxiaohuang', 2);
