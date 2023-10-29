543a30b7bb583ced4b285c75c6c8baeacbe100a1
ligulfzhou <ligulfzhou53@gmail.com>
2023-10-29 20:12:31 +0800

    deploy script

--- deploy-ovh.sh
+++ deploy-ovh.sh
@@ -2,5 +2,8 @@ ssh ovh << EOFOVH
 cd /home/debian/erp-api
 git pull origin main
 /home/debian/.cargo/bin/cargo build -r
-/usr/bin/svc restart erp-9100
+/usr/bin/svc restart erp:erp-9100
+/usr/bin/svc restart erp:erp-9101
+/usr/bin/svc restart erp:erp-9102
+/usr/bin/svc restart erp:erp-9103
 EOFOVH
--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,10 @@ ssh ovh << EOFOVH
 cd /home/debian/erp-api
 git pull origin main
 /home/debian/.cargo/bin/cargo build -r
-/usr/bin/svc restart erp-9100
+/usr/bin/svc restart erp:erp-9100
+/usr/bin/svc restart erp:erp-9100
+/usr/bin/svc restart erp:erp-9100
+/usr/bin/svc restart erp:erp-9100
 EOFOVH
 EOFHOSTHATCH
 
